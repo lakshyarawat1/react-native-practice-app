bf3fa01bb8fbcd7f0ebf5148192216f5a88ec855
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-09-23 16:20:54 +0530

    Added some text and images

--- client/App.js
+++ client/App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image , TextInput} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my first app.
-  
-      </Text>
+      <Text>This is my first app.</Text>
+      <Text>I am using expo and react native.</Text>
+      <Image
+        source={{
+          uri: "https://cdn.pixabay.com/photo/2023/08/19/17/36/bird-8200917_1280.jpg",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          width: "80%",
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
